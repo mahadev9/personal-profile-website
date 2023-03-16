053ded20069e4b648672b44e6396c5b8c2d96611
Mahadev Maitri <mahadevmaitri@gmail.com>
2023-03-16 08:26:49 -0400

    small changes

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Header from './Header'
-// import SEO from './SEO'
 
 export default function HomePage () {
   return (
     <div>
-      {/* <SEO /> */}
-      <Header />
     </div>
   )
 }
